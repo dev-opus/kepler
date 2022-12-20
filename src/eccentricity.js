bac4439920cd8e77073764d940c11a3d4636c4d8
victor orlunda <vicorlunda@gmail.com>
2022-12-20 05:44:46 +0100

    modify return value types

--- src/eccentricity.js
+++ src/eccentricity.js
@@ -7,9 +7,9 @@ function calculator(a, b) {
 
   if (sub < 0) {
     return 'could not be determined because of negative sign in square-root';
+  } else {
+    return +Math.sqrt(sub).toFixed(3);
   }
-
-  return Math.sqrt(sub).toFixed(3);
 }
 
 export default { calculator };
